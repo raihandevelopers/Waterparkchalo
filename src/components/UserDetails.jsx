4a81a6170c9a5bb3a90524eda4e31088b87d9eaf
Sai Shankar <saishankar15052005@gmail.com>
2024-11-19 23:49:13 +0530

    lksdl

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -5,32 +5,32 @@ export default function UserDetails() {
     const [userData, setUserData] = useState("");
 
 
-    useEffect(() => {
-        fetch("localhost:3000/change here", {
-            method: "POST",
-            crossDomain: true,
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-                "Access-Control-Allow-Origin": "*",
-            },
-            body: JSON.stringify({
-                token: window.localStorage.getItem("token"),
-            }),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data, "userData");
+    // useEffect(() => {
+    //     fetch("localhost:3000/change here", {
+    //         method: "POST",
+    //         crossDomain: true,
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json",
+    //             "Access-Control-Allow-Origin": "*",
+    //         },
+    //         body: JSON.stringify({
+    //             token: window.localStorage.getItem("token"),
+    //         }),
+    //     })
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             console.log(data, "userData");
 
-                setUserData(data.data);
+    //             setUserData(data.data);
 
-                if (data.data === "token expired") {
-                    alert("Token expired login again");
-                    window.localStorage.clear();
-                    window.location.href = "./sign-in";
-                }
-            });
-    }, []);
+    //             if (data.data === "token expired") {
+    //                 alert("Token expired login again");
+    //                 window.localStorage.clear();
+    //                 window.location.href = "./sign-in";
+    //             }
+    //         });
+    // }, []);
 
     return <UserHome userData={userData} />;
 }
